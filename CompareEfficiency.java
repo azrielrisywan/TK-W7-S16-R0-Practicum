2dececf7764b1423d7b4a543e5f46ef1cfb77bcf
Azriel Risywan Attaufiqi <azriel.risywan@pactindo.com>
2025-04-26 17:37:51 +0700

    update CompareEfficiency

--- CompareEfficiency.java
+++ CompareEfficiency.java
@@ -1,140 +1,281 @@
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-class Order {
-    private String orderId;
-    private String item;
-    private double price;
+// Class Mahasiswa untuk menyimpan data mahasiswa
+class Mahasiswa {
+    private String nim;
+    private String nama;
 
-    public Order(String orderId, String item, double price) {
-        this.orderId = orderId;
-        this.item = item;
-        this.price = price;
+    public Mahasiswa(String nim, String nama) {
+        this.nim = nim;
+        this.nama = nama;
     }
 
-    public String getOrderId() {
-        return orderId;
+    public String getNim() {
+        return nim;
     }
 
     @Override
     public String toString() {
-        return "Order{" + "orderId='" + orderId + "', item='" + item + "', price=" + price + "}";
+        return "Mahasiswa{nim='" + nim + "', nama='" + nama + "'}";
     }
 }
 
-public class CompareEfficiency {
-    public static void main(String[] args) {
-        int numOrders = 10000;
-        long startTime, endTime;
+// Class Node untuk implementasi manual LinkedList
+class Node {
+    private Mahasiswa data;
+    private Node next;
 
-        // Stack Implementation
-        Stack<Order> stack = new Stack<>();
-        System.out.println("\nStack Performance:");
+    public Node(Mahasiswa data) {
+        this.data = data;
+        this.next = null;
+    }
 
-        // Adding orders
-        startTime = System.nanoTime();
-        for (int i = 0; i < numOrders; i++) {
-            stack.push(new Order("S" + i, "Item" + i, 10.0 + i));
+    public Mahasiswa getData() {
+        return data;
+    }
+
+    public void setData(Mahasiswa data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+
+// Class CustomLinkedList untuk implementasi manual, mengimplementasikan Iterable
+class CustomLinkedList implements Iterable<Mahasiswa> {
+    private Node head;
+    private Node tail;
+
+    public CustomLinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void add(Mahasiswa mahasiswa) {
+        Node newNode = new Node(mahasiswa);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
         }
-        endTime = System.nanoTime();
-        System.out.println("Add " + numOrders + " orders: " +
-                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
 
-        // Processing orders
-        startTime = System.nanoTime();
+    public Mahasiswa removeFirst() {
+        if (head == null) {
+            return null;
+        }
+        Mahasiswa data = head.getData();
+        head = head.getNext();
+        if (head == null) {
+            tail = null;
+        }
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public Iterator<Mahasiswa> iterator() {
+        return new Iterator<Mahasiswa>() {
+            private Node current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public Mahasiswa next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Mahasiswa data = current.getData();
+                current = current.getNext();
+                return data;
+            }
+        };
+    }
+}
+
+// Class untuk mengukur performa Stack
+class StackPerformance {
+    private Stack<Mahasiswa> stack;
+    private int numItems;
+
+    public StackPerformance(int numItems) {
+        this.stack = new Stack<>();
+        this.numItems = numItems;
+    }
+
+    public void measureAddPerformance() {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numItems; i++) {
+            stack.push(new Mahasiswa("S" + i, "Mahasiswa" + i));
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Add " + numItems + " items: " +
+                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+
+    public void measureProcessPerformance() {
+        long startTime = System.nanoTime();
         while (!stack.isEmpty()) {
             stack.pop();
         }
-        endTime = System.nanoTime();
-        System.out.println("Process " + numOrders + " orders: " +
+        long endTime = System.nanoTime();
+        System.out.println("Process " + numItems + " items: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
 
-        // Queue Implementation
-        Queue<Order> queue = new ArrayDeque<>();
-        System.out.println("\nQueue Performance:");
-
-        // Adding orders
-        startTime = System.nanoTime();
-        for (int i = 0; i < numOrders; i++) {
-            queue.offer(new Order("Q" + i, "Item" + i, 10.0 + i));
+    public void measureSearchPerformance(String searchNim) {
+        // Repopulate for search
+        for (int i = 0; i < numItems; i++) {
+            stack.push(new Mahasiswa("S" + i, "Mahasiswa" + i));
         }
-        endTime = System.nanoTime();
-        System.out.println("Add " + numOrders + " orders: " +
+        long startTime = System.nanoTime();
+        for (Mahasiswa m : stack) {
+            if (m.getNim().equals(searchNim)) {
+                break;
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Stack search: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+}
 
-        // Processing orders
-        startTime = System.nanoTime();
+// Class untuk mengukur performa Queue
+class QueuePerformance {
+    private Queue<Mahasiswa> queue;
+    private int numItems;
+
+    public QueuePerformance(int numItems) {
+        this.queue = new LinkedList<>(); // Menggunakan LinkedList untuk Queue
+        this.numItems = numItems;
+    }
+
+    public void measureAddPerformance() {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numItems; i++) {
+            queue.offer(new Mahasiswa("Q" + i, "Mahasiswa" + i));
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Add " + numItems + " items: " +
+                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+
+    public void measureProcessPerformance() {
+        long startTime = System.nanoTime();
         while (!queue.isEmpty()) {
             queue.poll();
         }
-        endTime = System.nanoTime();
-        System.out.println("Process " + numOrders + " orders: " +
+        long endTime = System.nanoTime();
+        System.out.println("Process " + numItems + " items: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
 
-        // LinkedList Implementation
-        LinkedList<Order> linkedList = new LinkedList<>();
-        System.out.println("\nLinkedList Performance:");
-
-        // Adding orders
-        startTime = System.nanoTime();
-        for (int i = 0; i < numOrders; i++) {
-            linkedList.add(new Order("L" + i, "Item" + i, 10.0 + i));
+    public void measureSearchPerformance(String searchNim) {
+        // Repopulate for search
+        for (int i = 0; i < numItems; i++) {
+            queue.offer(new Mahasiswa("Q" + i, "Mahasiswa" + i));
         }
-        endTime = System.nanoTime();
-        System.out.println("Add " + numOrders + " orders: " +
+        long startTime = System.nanoTime();
+        for (Mahasiswa m : queue) {
+            if (m.getNim().equals(searchNim)) {
+                break;
+            }
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Queue search: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+}
 
-        // Processing orders
-        startTime = System.nanoTime();
+// Class untuk mengukur performa CustomLinkedList
+class CustomLinkedListPerformance {
+    private CustomLinkedList linkedList;
+    private int numItems;
+
+    public CustomLinkedListPerformance(int numItems) {
+        this.linkedList = new CustomLinkedList();
+        this.numItems = numItems;
+    }
+
+    public void measureAddPerformance() {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < numItems; i++) {
+            linkedList.add(new Mahasiswa("L" + i, "Mahasiswa" + i));
+        }
+        long endTime = System.nanoTime();
+        System.out.println("Add " + numItems + " items: " +
+                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+
+    public void measureProcessPerformance() {
+        long startTime = System.nanoTime();
         while (!linkedList.isEmpty()) {
             linkedList.removeFirst();
         }
-        endTime = System.nanoTime();
-        System.out.println("Process " + numOrders + " orders: " +
+        long endTime = System.nanoTime();
+        System.out.println("Process " + numItems + " items: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
 
-        // Search performance comparison
-        // Re-populate for search test
-        for (int i = 0; i < numOrders; i++) {
-            stack.push(new Order("S" + i, "Item" + i, 10.0 + i));
-            queue.offer(new Order("Q" + i, "Item" + i, 10.0 + i));
-            linkedList.add(new Order("L" + i, "Item" + i, 10.0 + i));
+    public void measureSearchPerformance(String searchNim) {
+        // Repopulate for search
+        for (int i = 0; i < numItems; i++) {
+            linkedList.add(new Mahasiswa("L" + i, "Mahasiswa" + i));
         }
-
-        String searchId = "S" + (numOrders - 1);
-        System.out.println("\nSearch Performance (searching last order):");
-
-        // Stack search
-        startTime = System.nanoTime();
-        for (Order order : stack) {
-            if (order.getOrderId().equals(searchId)) {
+        long startTime = System.nanoTime();
+        for (Mahasiswa m : linkedList) {
+            if (m.getNim().equals(searchNim)) {
                 break;
             }
         }
-        endTime = System.nanoTime();
-        System.out.println("Stack search: " +
+        long endTime = System.nanoTime();
+        System.out.println("CustomLinkedList search: " +
                 TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+    }
+}
 
-        // Queue search
-        startTime = System.nanoTime();
-        for (Order order : queue) {
-            if (order.getOrderId().equals(searchId)) {
-                break;
-            }
-        }
-        endTime = System.nanoTime();
-        System.out.println("Queue search: " +
-                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+// Main class
+public class CompareEfficiency {
+    public static void main(String[] args) {
+        int numItems = 10000;
 
-        // LinkedList search
-        startTime = System.nanoTime();
-        for (Order order : linkedList) {
-            if (order.getOrderId().equals(searchId)) {
-                break;
-            }
-        }
-        endTime = System.nanoTime();
-        System.out.println("LinkedList search: " +
-                TimeUnit.NANOSECONDS.toMicros(endTime - startTime) + " microseconds");
+        // Stack Performance
+        System.out.println("Stack Performance:");
+        StackPerformance stackPerf = new StackPerformance(numItems);
+        stackPerf.measureAddPerformance();
+        stackPerf.measureProcessPerformance();
+
+        // Queue Performance
+        System.out.println("\nQueue Performance:");
+        QueuePerformance queuePerf = new QueuePerformance(numItems);
+        queuePerf.measureAddPerformance();
+        queuePerf.measureProcessPerformance();
+
+        // Custom LinkedList Performance
+        System.out.println("\nCustomLinkedList Performance:");
+        CustomLinkedListPerformance customLLPerf = new CustomLinkedListPerformance(numItems);
+        customLLPerf.measureAddPerformance();
+        customLLPerf.measureProcessPerformance();
+
+        // Search Performance
+        String searchNim = "S" + (numItems - 1);
+        System.out.println("\nSearch Performance (searching last item):");
+        stackPerf.measureSearchPerformance(searchNim);
+        queuePerf.measureSearchPerformance(searchNim);
+        customLLPerf.measureSearchPerformance(searchNim);
     }
 }
